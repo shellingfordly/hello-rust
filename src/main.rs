7392a4d66156202a4b3236db20600e8d94715348
shellingfordly <shellingfordly@qq.com>
2024-02-23 17:12:13 +0800

    feat: add git push

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,23 @@
-use git2::{FetchOptions, RemoteCallbacks, Repository};
+use std::env;
 use std::fs;
+mod git;
+use git::{git_clone, git_commit, git_pull, git_push};
 
-fn git_clone(repo_url: &str, local_path: &str) -> bool {
-    match Repository::clone(repo_url, local_path) {
-        Ok(_repo) => {
-            println!("Repository cloned successfully");
-            return true;
-        }
+pub fn get_path() -> &'static str {
+    let current_dir = match env::current_dir() {
+        Ok(path) => path.join("templates/").to_string_lossy().into_owned(),
         Err(err) => {
-            eprintln!("Error cloning repository: {}", err);
-            return false;
-        }
-    }
-}
-
-fn git_pull(local_path: &str) {
-    let repo = match Repository::open(local_path) {
-        Ok(repo) => repo,
-        Err(_) => {
-            // 远程仓库 URL
-            let remote_url = "https://github.com/your-username/your-repo.git";
-
-            // 创建新的仓库并从远程克隆
-            git2::Repository::clone(remote_url, local_path).expect("Failed to clone from remote")
+            eprintln!("Failed to get current directory: {}", err);
+            String::new() // Return an empty string or handle the error differently
         }
     };
-    // 获取远程仓库
-    let remote_name = "origin"; // 远程仓库的名称
-    let mut remote: git2::Remote = repo
-        .find_remote(remote_name)
-        .expect("Failed to find remote");
 
-    // 设置回调函数（例如，用于认证）
-    let mut callbacks = RemoteCallbacks::new();
-    // 这里可以添加认证信息等回调函数
-
-    // 执行拉取操作
-    let mut fetch_options = FetchOptions::new();
-    fetch_options.remote_callbacks(callbacks);
-
-    remote
-        .fetch::<&str>(&[], Some(&mut fetch_options), None)
-        .expect("Failed to fetch from remote");
+    Box::leak(current_dir.into_boxed_str())
 }
 
 fn main() {
-    let repo_url = "https://github.com/shellingfordly/hello-rust.git";
-    let local_path = "/Users/apple/Projects/hello-rust/templates/";
+    let repo_url: &str = "https://github.com/shellingfordly/hello-rust.git";
+    let local_path: &str = get_path();
 
     let mut is_exist = false;
 
@@ -56,7 +27,6 @@ fn main() {
             println!("Folder does not exist at path: {}", local_path);
         }
     }
-    println!("is_exist: {}", is_exist);
 
     if !is_exist {
         let clone_status = git_clone(repo_url, local_path);
@@ -67,5 +37,9 @@ fn main() {
         }
     }
 
-    git_pull(local_path);
+    git_pull(repo_url, local_path);
+
+    git_commit(local_path);
+
+    git_push(local_path);
 }
